454dca2a08b70ae767cb778715ec2e36d76bda80
lwatson <lwatson@wikimedia.org>
2025-10-16 09:40:55 -0400

    ImageBrowsing: gate the feature to exclude unsupported browsers
    The ImageBrowsing prototype will exclude these
    unsupported browsers:
    - iOS 14 and below
    - Android 9 and below
    
    Leading to this decision, we performed on-device
    browser compatibility testing on iOS 12 and 15.
    Unresolved CORS issue in iOS 12 locally resulted in
    infobox images not loading in the Carousel and
    DetailView.
    
    More info about browser support policy is in task
    https://phabricator.wikimedia.org/T406379#11293706
    
    Bug: T406379
    Change-Id: I52ced227f7648d7f98e1d06a51d41eff1ad41799

--- resources/ext.readerExperiments.imageBrowsing/init.js
+++ resources/ext.readerExperiments.imageBrowsing/init.js
@@ -6,6 +6,52 @@ const Vue = require( 'vue' );
 const App = require( './App.vue' );
 const instrumentation = require( './plugins/instrumentationPlugin.js' );
 
+/**
+ * Check if the current browser is supported.
+ * Exclude iOS 14 and below, and Android 9 and below from
+ * the image browsing experiment.
+ *
+ * User agent is not always reliable, so if we can't determine
+ * the user agent then show the experimental feature.
+ *
+ * Examples of user agents: "iPhone OS 15_8", "Android 10", "Windows 10"
+ *
+ * @return {boolean}
+ */
+function isSupportedBrowser() {
+	// Capture groups: 1 = iOS version, 2 = Android version
+	const deviceRegExp = /iP(?:hone|ad|od).+OS (\d+)|Android (\d+)/;
+	const match = navigator.userAgent.match( deviceRegExp );
+
+	// If it's not iOS or Android, then allow all other browsers.
+	if ( !match ) {
+		return true;
+	}
+
+	const iOSVersion = match[ 1 ];
+	if ( iOSVersion ) {
+		// Exclude iOS 14 and below.
+		return parseInt( iOSVersion, 10 ) >= 15;
+	}
+
+	const androidVersion = match[ 2 ];
+	if ( androidVersion ) {
+		// Exclude Android 9 and below.
+		return parseInt( androidVersion, 10 ) >= 10;
+	}
+
+	// Versions are undetermined, allow by default.
+	return true;
+}
+
+// Early exit if browser is not supported
+if ( !isSupportedBrowser() ) {
+	// eslint-disable-next-line no-console
+	console.log( '[ImageBrowsing] Browser not supported; skipping initialization.' );
+	// Exit early - do not initialize the app
+	return;
+}
+
 // Instrumentation setup.
 const EXPERIMENT_NAME = 'fy2025-26-we3.1-image-browsing-ab-test';
 const SCHEMA_NAME = '/analytics/product_metrics/web/base/1.4.3';
