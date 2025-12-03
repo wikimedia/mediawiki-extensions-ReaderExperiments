07c9e2fe2e5fc23cff91622f6082ee255a4ea2f6
Brooke Vibber <bvibber@wikimedia.org>
2025-12-03 11:33:41 -0800

    StickyHeaders: possible fix for 'bouncing' on sticky offset
    Could occasionally reproduce a state where it would change
    sticky state on a header at high frequency when scrolling
    to a particular position.
    
    Have only been able to reproduce it on beta so far, uncertain
    if this resolves it.
    
    Redid the state management of `intersectingHeadings` from
    an array that could grow in definitely to a Set that's
    managed more carefully.
    
    Also changed the pre-decided offscreen check from "> 0"
    to ">= 0" just to make sure no stray fractional pixels
    spill over.
    
    And teaked the state management to remove the CSS bits and
    the state Set removal at the same time, seems clearer.
    
    ...and now also making sure we update the 'top' when scrolling
    fast and new things come into view.
    
    Bug: T409325
    Change-Id: I8a5a938703f875fe2877beeb1fc563865e32951e

--- resources/ext.readerExperiments.stickyHeaders.common/init.js
+++ resources/ext.readerExperiments.stickyHeaders.common/init.js
@@ -32,16 +32,43 @@ function checkHash() {
 
 function setupStickyHeaders( headings ) {
 	// Observe headings whose content intersects the viewport
-	const intersectingHeadings = [];
+	const intersectingHeadings = new Set();
+
+	const setStickyHeading = ( heading ) => {
+		intersectingHeadings.add( heading );
+		heading.classList.add( 'ext-readerExperiments-stickyHeaders' );
+	};
+	const unsetStickyHeading = ( heading ) => {
+		intersectingHeadings.delete( heading );
+		heading.classList.remove( 'ext-readerExperiments-stickyHeaders' );
+		heading.style.removeProperty( 'top' );
+	};
+
+	const setHeadingPosition = ( heading ) => {
+		const content = heading.nextElementSibling;
+		const contentBottom = content.getBoundingClientRect().bottom;
+		const headingHeight = heading.getBoundingClientRect().height;
+		if ( contentBottom >= 0 ) {
+			heading.style.top = Math.min( 0, contentBottom - headingHeight ) + 'px';
+		} else {
+			// The heading has scrolled up offscreen, we can de-sticky it safely
+			// If it comes back on screen the observers will catch it and we'll
+			// correct its position.
+			unsetStickyHeading( heading );
+		}
+	};
+
 	// eslint-disable-next-line compat/compat
-	const insersectionObserver = new IntersectionObserver( ( sections ) => {
+	const intersectionObserver = new IntersectionObserver( ( sections ) => {
 		sections.forEach( ( section ) => {
 			const heading = section.target.previousSibling;
 			if ( section.isIntersecting ) {
-				heading.classList.add( 'ext-readerExperiments-stickyHeaders' );
-				intersectingHeadings.push( heading );
-			} else {
-				heading.classList.remove( 'ext-readerExperiments-stickyHeaders' );
+				if ( !intersectingHeadings.has( heading ) ) {
+					setStickyHeading( heading );
+					setHeadingPosition( heading );
+				}
+			} else if ( intersectingHeadings.has( heading ) ) {
+				unsetStickyHeading( heading );
 			}
 		} );
 	} );
@@ -50,24 +77,14 @@ function setupStickyHeaders( headings ) {
 		const background = document.createElement( 'div' );
 		background.classList.add( 'ext-readerExperiments-stickyHeaders-background' );
 		heading.insertBefore( background, heading.firstChild );
-		insersectionObserver.observe( heading.nextSibling );
+		intersectionObserver.observe( heading.nextSibling );
 	} );
 
 	// Because headings may be of different size, we need to make sure they don't overlap,
 	// so we'll make sure they don't remain sticky at the top for longer then the height of
 	// the content of their section (in order not to interfere/overlap with the next section)
 	const scrollHandler = () => {
-		intersectingHeadings.forEach( ( heading ) => {
-			const content = heading.nextElementSibling;
-			const contentBottom = content.getBoundingClientRect().bottom;
-			const headingHeight = heading.getBoundingClientRect().height;
-			if ( contentBottom > 0 ) {
-				heading.style.top = Math.min( 0, contentBottom - headingHeight ) + 'px';
-			} else {
-				heading.style.removeProperty( 'top' );
-				intersectingHeadings.splice( intersectingHeadings.indexOf( heading ), 1 );
-			}
-		} );
+		intersectingHeadings.forEach( setHeadingPosition );
 	};
 	window.addEventListener( 'scroll', mw.util.throttle( scrollHandler, 100 ) );
 
